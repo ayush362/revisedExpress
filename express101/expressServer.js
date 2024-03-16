15ff30d1fa7ae9520f864607adbb177c31fd7da5
Ayush khatri <ayushkhatri362@gmail.com>
2024-03-16 15:41:06 +0530

    Serving static files in express

--- express101/expressServer.js
+++ express101/expressServer.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.all("*", (req, res) => {
+app.use(express.static("public"));
+
+app.all("/", (req, res) => {
     // Express handles the basic headers (status code,mime-type)
     // express handles the ends
-    res.send(`<h1>This is the home page</h1>`);
+    // res.send(`<h1>This is the home page</h1>`);
+    res.sendFile(path.join(__dirname + "/node/node.html"));
 });
 
-app.listen(3000,()=>{
+app.all("*", (req, res) => {
+    res.send(`<h1>404: Page not found</h1>`);
+});
+app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
